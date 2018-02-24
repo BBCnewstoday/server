aeb2e28b317c69ff72f138c3c59d17218497d46b
Sulf Mesmerize <sulf.the.great@gmail.com>
2018-02-24 19:15:43 +0200

    Revert "New progressive database release."
    This reverts commit be904078851f5549ea132876f51fdee506b46ce2.

--- sql/logs.sql
+++ sql/logs.sql
@@ -1,18 +1,21 @@
--- --------------------------------------------------------
--- Хост:                         127.0.0.1
--- Server version:               5.5.53 - MySQL Community Server (GPL)
--- Server OS:                    Win32
--- HeidiSQL Версия:              9.3.0.4998
--- --------------------------------------------------------
+/*
+SQLyog Ultimate
+MySQL - 5.5.53-0+deb8u1-log : Database - logs
+*********************************************************************
+*/
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET NAMES utf8mb4 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+/*Table structure for table `logs_battleground` */
 
--- Dumping structure for table logs.logs_battleground
 DROP TABLE IF EXISTS `logs_battleground`;
-CREATE TABLE IF NOT EXISTS `logs_battleground` (
+
+CREATE TABLE `logs_battleground` (
   `time` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
   `bgid` int(11) DEFAULT NULL,
   `bgtype` int(11) DEFAULT NULL,
@@ -25,12 +28,11 @@ CREATE TABLE IF NOT EXISTS `logs_battleground` (
   `honorableKills` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `logs_behavior` */
 
-
--- Dumping structure for table logs.logs_behavior
 DROP TABLE IF EXISTS `logs_behavior`;
-CREATE TABLE IF NOT EXISTS `logs_behavior` (
+
+CREATE TABLE `logs_behavior` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
   `account` int(10) unsigned NOT NULL,
   `detection` varchar(255) NOT NULL,
@@ -38,12 +40,11 @@ CREATE TABLE IF NOT EXISTS `logs_behavior` (
   KEY `id` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `logs_characters` */
 
-
--- Dumping structure for table logs.logs_characters
 DROP TABLE IF EXISTS `logs_characters`;
-CREATE TABLE IF NOT EXISTS `logs_characters` (
+
+CREATE TABLE `logs_characters` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` enum('LostSocket','Create','Delete','Login','Logout','') NOT NULL DEFAULT '',
   `guid` int(11) NOT NULL DEFAULT '0',
@@ -56,12 +57,11 @@ CREATE TABLE IF NOT EXISTS `logs_characters` (
   KEY `clientHash` (`clientHash`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `logs_chat` */
 
-
--- Dumping structure for table logs.logs_chat
 DROP TABLE IF EXISTS `logs_chat`;
-CREATE TABLE IF NOT EXISTS `logs_chat` (
+
+CREATE TABLE `logs_chat` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` enum('Say','Whisp','Group','Guild','Officer','Raid','BG','Chan','') NOT NULL DEFAULT '',
   `guid` int(11) NOT NULL DEFAULT '0',
@@ -73,12 +73,11 @@ CREATE TABLE IF NOT EXISTS `logs_chat` (
   KEY `target` (`target`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `logs_spamdetect` */
 
-
--- Dumping structure for table logs.logs_spamdetect
 DROP TABLE IF EXISTS `logs_spamdetect`;
-CREATE TABLE IF NOT EXISTS `logs_spamdetect` (
+
+CREATE TABLE `logs_spamdetect` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `accountId` int(11) DEFAULT '0',
   `guid` int(11) NOT NULL DEFAULT '0',
@@ -87,12 +86,11 @@ CREATE TABLE IF NOT EXISTS `logs_spamdetect` (
   KEY `guid` (`guid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `logs_trade` */
 
-
--- Dumping structure for table logs.logs_trade
 DROP TABLE IF EXISTS `logs_trade`;
-CREATE TABLE IF NOT EXISTS `logs_trade` (
+
+CREATE TABLE `logs_trade` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `type` enum('AuctionBid','AuctionBuyout','SellItem','GM','Mail','QuestMaxLevel','Quest','Loot','Trade','') NOT NULL DEFAULT '',
   `sender` int(11) unsigned NOT NULL DEFAULT '0',
@@ -105,14 +103,13 @@ CREATE TABLE IF NOT EXISTS `logs_trade` (
   KEY `receiver` (`receiver`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `logs_transactions` */
 
-
--- Dumping structure for table logs.logs_transactions
 DROP TABLE IF EXISTS `logs_transactions`;
-CREATE TABLE IF NOT EXISTS `logs_transactions` (
+
+CREATE TABLE `logs_transactions` (
   `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `type` enum('Bid','Buyout','PlaceAuction','Trade','Mail','MailCOD') DEFAULT NULL,
+  `type` enum('Bid','Buyout','Trade','Mail','MailCOD','') NOT NULL DEFAULT '',
   `guid1` int(11) unsigned NOT NULL DEFAULT '0',
   `money1` int(11) unsigned NOT NULL DEFAULT '0',
   `spell1` int(11) unsigned NOT NULL DEFAULT '0',
@@ -125,45 +122,41 @@ CREATE TABLE IF NOT EXISTS `logs_transactions` (
   KEY `guid1` (`guid1`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `logs_trashcharacters` */
 
-
--- Dumping structure for table logs.logs_trashcharacters
 DROP TABLE IF EXISTS `logs_trashcharacters`;
-CREATE TABLE IF NOT EXISTS `logs_trashcharacters` (
+
+CREATE TABLE `logs_trashcharacters` (
   `guid` int(10) unsigned NOT NULL,
   `data` varchar(255) NOT NULL,
   `cluster` tinyint(3) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`guid`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
+/*Table structure for table `smartlog_creature` */
 
+DROP TABLE IF EXISTS `smartlog_creature`;
 
--- Dumping structure for table logs.migrations
+CREATE TABLE `smartlog_creature` (
+  `time` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `type` ENUM('Death','LongCombat','ScriptInfo','') NOT NULL DEFAULT '',
+  `entry` INT(11) NOT NULL DEFAULT '0',
+  `guid` INT(11) NOT NULL DEFAULT '0',
+  `specifier` VARCHAR(255) NOT NULL DEFAULT '',
+  `combatTime` INT(11) NOT NULL DEFAULT '0',  
+  `content` VARCHAR(255) NOT NULL DEFAULT '',
+  KEY `entry` (`entry`)
+) ENGINE=INNODB DEFAULT CHARSET=utf8;
+
+/*Table structure for table `migrations` */
+
 DROP TABLE IF EXISTS `migrations`;
-CREATE TABLE IF NOT EXISTS `migrations` (
+
+CREATE TABLE `migrations` (
   `id` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
--- Data exporting was unselected.
-
-
--- Dumping structure for table logs.smartlog_creature
-DROP TABLE IF EXISTS `smartlog_creature`;
-CREATE TABLE IF NOT EXISTS `smartlog_creature` (
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `type` enum('Death','LongCombat','ScriptInfo','') NOT NULL DEFAULT '',
-  `entry` int(11) NOT NULL DEFAULT '0',
-  `guid` int(11) NOT NULL DEFAULT '0',
-  `specifier` varchar(255) NOT NULL DEFAULT '',
-  `combatTime` int(11) NOT NULL DEFAULT '0',
-  `content` varchar(255) NOT NULL DEFAULT '',
-  KEY `entry` (`entry`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- Data exporting was unselected.
-/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
-/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
